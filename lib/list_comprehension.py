e8d6126d52b49020616bf4cc9108251051b2d28c
Nikki Little <nikkilittle13@gmail.com>
2024-04-16 09:43:35 -0400

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    new_list = [n for n in num_list if n % 2 == 0]
+    return new_list
     pass
 
 def make_exclamation(sentence_list):
+    if sentence_list.count == 0:
+        return []
+    else:
+        new_sentence_list = [word + '!' for word in sentence_list]
+        return new_sentence_list
     pass
